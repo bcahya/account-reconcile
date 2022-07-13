b3b5404ddbf8db654517b804b32f7d701246ba33
rifdyf <rifdyf@gmail.com>
2022-07-13 14:06:01 +0700

    delete console log

--- account_reconciliation_widget/static/src/js/reconciliation/reconciliation_model.js
+++ account_reconciliation_widget/static/src/js/reconciliation/reconciliation_model.js
@@ -1625,11 +1625,9 @@ odoo.define("account.ReconciliationModel", function (require) {
             }
 
             var shift_label = line.st_line.name
-            console.log(shift_label)
             if (shift_label){
                 shift_label = shift_label.split(":")
-                shift_label = shift_label[shift_label.length-1]
-                console.log(shift_label)
+                shift_labels = shift_label[shift_label.length-1]
             }
             var prop = {
                 id: _.uniqueId("createLine"),
